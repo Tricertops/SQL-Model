cc0ce9e17eeadc40d03aac015f59a80c5fa43ea1
Martin Kiss <martin.kiss@me.com>
2014-05-19 18:56:36 +0200

    Add tests for the lib

---
+++ Tests/SQLEntityTest.m
@@ -0,0 +1,33 @@
+//
+//  SQL_Model_Tests.m
+//  SQL Model Tests
+//
+//  Created by Martin Kiss on 19.5.14.
+//  Copyright (c) 2014 Triceratops. All rights reserved.
+//
+
+@import XCTest;
+
+
+
+@interface SQL_Model_Tests : XCTestCase
+
+@end
+
+
+
+
+
+@implementation SQL_Model_Tests
+
+
+
+- (void)testExample {
+    
+}
+
+
+
+@end
+
+
