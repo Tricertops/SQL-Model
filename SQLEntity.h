4257e4908b1dd7d617251bb6f45f1b7ae6529a6b
Martin Kiss <martin.kiss@me.com>
2014-05-19 18:45:18 +0200

    Create project

---
+++ SQLEntity.h
@@ -0,0 +1,13 @@
+//
+//  SQLEntity.h
+//  SQL Model
+//
+//  Created by Martin Kiss on 19.5.14.
+//  Copyright (c) 2014 Triceratops. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SQLEntity : NSObject
+
+@end
---
+++ SQLModel.h
@@ -0,0 +1,13 @@
+//
+//  SQL_Model.h
+//  SQL Model
+//
+//  Created by Martin Kiss on 19.5.14.
+//  Copyright (c) 2014 Triceratops. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SQL_Model : NSObject
+
+@end
