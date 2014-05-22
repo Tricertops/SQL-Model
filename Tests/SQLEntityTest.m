35bc04c8934961254c74f8e671029c6addd6fc1a
Martin Kiss <martin.kiss@me.com>
2014-05-22 10:45:49 +0200

    Test generating tableNames, passing

--- Tests/SQLEntityTest.m
+++ Tests/SQLEntityTest.m
@@ -24,10 +24,24 @@
 
 
 
+
 - (void)test_automagicInstanceName {
     XCTAssertEqualObjects([SQLTestBottle instanceName], @"testBottle");
     XCTAssertEqualObjects([SQLTestFlask instanceName], @"testFlask");
+    XCTAssertEqualObjects([SQLEntity sql_instanceNameFromClassName:@"ABCDevil"], @"devil");
 }
+
+
+- (void)test_automagicTableName {
+    XCTAssertEqualObjects([SQLTestBottle tableName], @"testBottles");
+    XCTAssertEqualObjects([SQLTestFlask tableName], @"testFlasks");
+    XCTAssertEqualObjects([SQLEntity sql_tableNameFromInstanceName:@"box"], @"boxes");
+    XCTAssertEqualObjects([SQLEntity sql_tableNameFromInstanceName:@"clearGlass"], @"clearGlasses");
+    XCTAssertEqualObjects([SQLEntity sql_tableNameFromInstanceName:@"badChild"], @"badChildren");
+    XCTAssertEqualObjects([SQLEntity sql_tableNameFromInstanceName:@"goodPerson"], @"goodPeople");
+}
+
+
 
 
 
